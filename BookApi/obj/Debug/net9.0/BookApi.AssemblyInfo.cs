0c9378e4545546276c38ed4c5da579a1894232e9
Nadun677 <nadunnimansha067@gmail.com>
2026-03-12 20:28:16 +0530

    Initial commit - Angular + ASP.NET Book Management CRUD

--- BookApi/obj/Debug/net9.0/BookApi.AssemblyInfo.cs
+++ BookApi/obj/Debug/net9.0/BookApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a17a0bdefd4e3055c120615a811c31339a283844")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
